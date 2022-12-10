16b7e2669b3c54dd4bcd6894aa7b9b46a1e2c6d4
neel123go <ayounpaul1029@gmail.com>
2022-12-11 02:28:21 +0600

    sign in & sign up responsive done

--- src/components/Modal/CreateAccount.js
+++ src/components/Modal/CreateAccount.js
@@ -4,6 +4,7 @@ import { FaFacebook } from 'react-icons/fa';
 import { FcGoogle } from 'react-icons/fc';
 import AccountImage from '../../Images/create-account-image.png';
 import SignIn from './SignIn';
+import './Modal.css';
 
 const JoinGroupModal = ({ user, setUser }) => {
     const [error, setError] = useState('');
@@ -35,18 +36,18 @@ const JoinGroupModal = ({ user, setUser }) => {
 
     return (
         <>
-            <div className="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true" >
-                <div className="modal-dialog modal-lg modal-dialog-centered mx-auto" style={{ width: '45%' }}>
-                    <div className="modal-content position-relative" style={{ borderRadius: '8px' }}>
-                        <RxCross2 className='p-1 position-absolute end-0 bg-light rounded-circle' style={{ top: '-40px', width: '24px', height: '24px', cursor: 'pointer' }} data-bs-dismiss="modal" aria-label="Close" />
-                        <div className="modal-header text-center py-2" style={{ backgroundColor: '#EFFFF4' }}>
+            <div className="modal mt-5 fade overflow-hidden" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true" >
+                <div className="modal-dialog mt-5 mt-md-0 modal-lg modal-dialog-centered mx-auto">
+                    <div className="modal-content mt-5 mt-md-0 position-relative" style={{ borderRadius: '8px' }}>
+                        <RxCross2 className='p-1 position-absolute d-none d-md-block end-0 bg-light rounded-circle' style={{ top: '-40px', width: '24px', height: '24px', cursor: 'pointer' }} data-bs-dismiss="modal" aria-label="Close" />
+                        <div className="modal-header text-center py-2 d-none d-md-block" style={{ backgroundColor: '#EFFFF4' }}>
                             <h5 className='text-success mt-1 mx-auto' style={{ fontSize: '14px' }}>Let's learn, share & inspire each other with our passion for computer engineering. Sign up now 🤘🏼</h5>
                         </div>
                         <div className="modal-body p-4">
-
                             <div className='d-flex justify-content-between mb-3'>
                                 <h3 style={{ fontSize: '24px', fontWeight: '700', color: '#000000' }}>Create Account</h3>
-                                <p className='mt-2' style={{ fontSize: '13px', color: '#3D3D3D', fontWeigth: '400' }}>Already have an account? <span data-bs-target="#staticBackdropSignIn" data-bs-toggle="modal" data-bs-dismiss="modal" style={{ color: '#2F6CE5', cursor: 'pointer' }}>Sign In</span></p>
+                                <RxCross2 className='p-1 d-md-none d-block position-absolute bg-dark text-light rounded-circle' style={{ width: '24px', right: '20px', height: '24px', cursor: 'pointer' }} data-bs-dismiss="modal" aria-label="Close" />
+                                <p className='mt-2 d-none d-md-block' style={{ fontSize: '13px', color: '#3D3D3D', fontWeigth: '400' }}>Already have an account? <span data-bs-target="#staticBackdropSignIn" data-bs-toggle="modal" data-bs-dismiss="modal" style={{ color: '#2F6CE5', cursor: 'pointer' }}>Sign In</span></p>
                             </div>
 
                             <div className='d-flex gap-4 justify-content-between align-items-center'>
@@ -66,12 +67,18 @@ const JoinGroupModal = ({ user, setUser }) => {
                                                 <input type="password" className="form-control rounded-0" ref={confirmPasswordRef} placeholder='Confirm Password' required />
                                             </div>
                                         </div>
-                                        <button type="submit" className="text-light rounded-pill mb-2 border-0 py-2" style={{ backgroundColor: '#2F6CE5', width: '100%' }}>Create Account</button>
+
+                                        <div className='d-md-grid gap-2 d-flex'>
+                                            <button type="submit" className="text-light rounded-pill mb-2 border-0 py-2 px-md-0 px-5" style={{ backgroundColor: '#2F6CE5' }}>Create Account</button>
+                                            <p data-bs-target="#staticBackdropSignIn" data-bs-toggle="modal" data-bs-dismiss="modal" style={{ color: '#495057', cursor: 'pointer', fontWeight: '600' }} className='mt-2 d-md-none d-block end-0 position-absolute me-4'>or, Sign In</p>
+                                        </div>
                                         <button type="button" className="text-dark bg-transparent rounded-3 my-2 py-2" style={{ width: '100%', border: '0.6px solid #D9D9DB', fontSize: '14px', fontWeight: '600' }}><FaFacebook style={{ marginRight: '10px' }} fontSize={20} fill="#1877F2" /> Sign up with Facebook</button>
                                         <button type="button" className="text-dark bg-transparent rounded-3 my-2 py-2" style={{ width: '100%', border: '0.6px solid #D9D9DB', fontSize: '14px', fontWeight: '600' }}><FcGoogle style={{ marginRight: '10px' }} fontSize={20} /> Sign up with Google</button>
+
+                                        <p className='mt-4 text-center' style={{ fontSize: '12px' }}>By signing up, you agree to our Terms & conditions, Privacy policy</p>
                                     </form>
                                 </div>
-                                <div style={{ width: '100%' }} className="d-flex flex-column justify-content-between">
+                                <div style={{ width: '100%' }} className="d-md-flex d-none flex-column justify-content-between">
                                     <img src={AccountImage} style={{ width: '90%' }} alt="" />
                                     <p className='mt-4' style={{ fontSize: '10px' }}>By signing up, you agree to our Terms & conditions, Privacy policy</p>
                                 </div>
--- src/components/Modal/SignIn.js
+++ src/components/Modal/SignIn.js
@@ -27,18 +27,20 @@ const SignIn = ({ user, setUser }) => {
     }
 
     return (
-        <div className="modal fade" id="staticBackdropSignIn" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabelSignIn" aria-hidden="true" >
-            <div className="modal-dialog modal-lg modal-dialog-centered mx-auto" style={{ width: '45%' }}>
-                <div className="modal-content position-relative" style={{ borderRadius: '8px' }}>
-                    <RxCross2 className='p-1 position-absolute end-0 bg-light rounded-circle' style={{ top: '-40px', width: '24px', height: '24px', cursor: 'pointer' }} data-bs-dismiss="modal" aria-label="Close" />
-                    <div className="modal-header text-center py-2" style={{ backgroundColor: '#EFFFF4' }}>
+        <div className="modal mt-5 mb-md-0 mb-5 fade overflow-hidden" id="staticBackdropSignIn" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabelSignIn" aria-hidden="true" >
+            <div className="modal-dialog mb-5 mb-md-0 mt-5 mt-md-0 modal-lg modal-dialog-centered mx-auto">
+                <div className="modal-content pb-5 pb-md-3 mt-5 mt-md-0 position-relative" style={{ borderRadius: '8px' }}>
+                    <RxCross2 className='p-1 position-absolute d-none d-md-block end-0 bg-light rounded-circle' style={{ top: '-40px', width: '24px', height: '24px', cursor: 'pointer' }} data-bs-dismiss="modal" aria-label="Close" />
+                    <div className="modal-header text-center py-2 d-none d-md-block" style={{ backgroundColor: '#EFFFF4' }}>
                         <h5 className='text-success mt-1 mx-auto' style={{ fontSize: '14px' }}>Let's learn, share & inspire each other with our passion for computer engineering. Sign up now 🤘🏼</h5>
                     </div>
-                    <div className="modal-body p-4">
-
+                    <div className="modal-body pb-5 pb-md-0 p-4">
                         <div className='d-flex justify-content-between mb-3'>
-                            <h3 style={{ fontSize: '24px', fontWeight: '700', color: '#000000' }}>Sign In</h3>
-                            <p className='mt-2' style={{ fontSize: '13px', color: '#3D3D3D', fontWeigth: '400' }}>Don’t have an account yet? <span data-bs-target="#staticBackdrop" data-bs-toggle="modal" data-bs-dismiss="modal" style={{ color: '#2F6CE5', cursor: 'pointer' }}>Create new for free!</span></p>
+                            <h3 style={{ fontSize: '24px', fontWeight: '700', color: '#000000' }}>Welcome back!</h3>
+
+                            <RxCross2 className='p-1 d-md-none d-block position-absolute bg-dark text-light rounded-circle' style={{ width: '24px', right: '20px', height: '24px', cursor: 'pointer' }} data-bs-dismiss="modal" aria-label="Close" />
+
+                            <p className='mt-2 d-none d-md-block' style={{ fontSize: '13px', color: '#3D3D3D', fontWeigth: '400' }}>Don’t have an account yet? <span data-bs-target="#staticBackdrop" data-bs-toggle="modal" data-bs-dismiss="modal" style={{ color: '#2F6CE5', cursor: 'pointer' }}>Create new for free!</span></p>
                         </div>
 
                         <div className='d-flex gap-4 justify-content-between align-items-center'>
@@ -52,13 +54,19 @@ const SignIn = ({ user, setUser }) => {
                                             <input type="password" className="form-control rounded-0" placeholder='Password' ref={passwordRef} required />
                                         </div>
                                     </div>
-                                    <button type="submit" className="text-light rounded-pill mb-2 border-0 py-2" style={{ backgroundColor: '#2F6CE5', width: '100%' }}>Sign In</button>
+
+                                    <div className='d-md-grid gap-2 d-flex'>
+                                        <button type="submit" className="text-light rounded-pill my-md-2 my-4 border-0 py-2 px-md-0 px-5" style={{ backgroundColor: '#2F6CE5' }}>Sign In</button>
+                                        <p data-bs-target="#staticBackdrop" data-bs-toggle="modal" data-bs-dismiss="modal" style={{ color: '#495057', cursor: 'pointer', fontWeight: '600' }} className='mt-2 d-md-none d-block end-0 position-absolute me-4'>or, Create Account</p>
+                                    </div>
+
                                     <button type="button" className="text-dark bg-transparent rounded-3 my-2 py-2" style={{ width: '100%', border: '0.6px solid #D9D9DB', fontSize: '14px', fontWeight: '600' }}><FaFacebook style={{ marginRight: '10px' }} fontSize={20} fill="#1877F2" /> Sign up with Facebook</button>
                                     <button type="button" className="text-dark bg-transparent rounded-3 my-2 py-2" style={{ width: '100%', border: '0.6px solid #D9D9DB', fontSize: '14px', fontWeight: '600' }}><FcGoogle style={{ marginRight: '10px' }} fontSize={20} /> Sign up with Google</button>
                                     <p className='text-center mt-2' style={{ fontWeight: '600', fontSize: '14px' }}>Forgot Password?</p>
                                 </form>
                             </div>
-                            <div style={{ width: '100%' }} className="d-flex flex-column justify-content-between">
+
+                            <div style={{ width: '100%' }} className="d-md-flex d-none flex-column justify-content-between">
                                 <img src={AccountImage} style={{ width: '90%' }} alt="" />
                                 <p className='mt-4' style={{ fontSize: '10px' }}>By signing up, you agree to our Terms & conditions, Privacy policy</p>
                             </div>
